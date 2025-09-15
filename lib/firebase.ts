b918d743116f396a28478bcf3943872ad894128c
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-09-15 22:32:37 +0530

    done the part of the firebase thing completly

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6p0MgbwikL4RMHhfC6k8ip02_89YvjYQ",
-  authDomain: "whatsap-clone-c889b.firebaseapp.com",
-  projectId: "whatsap-clone-c889b",
-  storageBucket: "whatsap-clone-c889b.firebasestorage.app",
-  messagingSenderId: "99979674018",
-  appId: "1:99979674018:web:ae857b37f2a376ea0072fb",
-  measurementId: "G-05VL4FVBZP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
